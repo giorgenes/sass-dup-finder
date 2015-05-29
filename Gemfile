6f12a97320ed19d218dfaafaa426d968d7a5e17c
Giorgenes <giorgenes@gmail.com>
2015-05-28 18:04:20 -0700

    adds gem files and dump script

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'http://rubygems.org'
+
+gem 'haml'
+gem 'sass'
+gem 'awesome_print'
--- dump-selectors.rb
+++ dump-selectors.rb
@@ -24,17 +24,13 @@ end
 files = {}
 rules = {}
 ARGV.each do |f|
-	files[f] = Set.new
-	rules[f] = {}
 	engine = Sass::Engine.new(File.new(f).read, :syntax => :scss)
 	engine.to_tree.children.each do |c|
 		if c.is_a?(Sass::Tree::RuleNode)
 			#ap c.rule
 			c.rule.each do |rule|
 				rule.tr(' ', '').split(",").each do |r|
-					files[f].add(r)
-					rules[f][r] = c
-					#puts r
+					puts r
 				end
 			end
 		end
@@ -42,24 +38,3 @@ ARGV.each do |f|
 end
 
 
-files.keys.combination(2) do |fl|
-	k = fl[0]
-	k2 = fl[1]
-	v = files[k]
-	v2 = files[k2]
-	i = v.intersection(v2)
-	unless i.empty? or k == k2
-		puts "#{k} with #{k2}"
-		puts "equal selectors"
-		ap i
-		i.each do |r|
-			a = node_to_set(rules[k][r])
-			b = node_to_set(rules[k2][r])
-			diff = (a + b) - (a & b)
-			puts "different rules for #{r}"
-			diff.each do |dd|
-				puts "    #{dd}"
-			end
-		end
-	end
-end
